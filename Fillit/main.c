49d113e2547c413c367a3e7261470dc6e0463dfa
jkuusist <jkuusist@student.hive.fi>
2019-11-21 13:42:05 +0200

    fixed a couple of typos

--- Fillit/main.c
+++ Fillit/main.c
@@ -6,11 +6,11 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 11:21:04 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/21 13:36:49 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/21 13:40:53 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "../Libft/libft.h"
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -20,10 +20,10 @@ int	fd;
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
-		write(2, "Usage: fillit target_file", 26);
+		write(2, "Usage: fillit target_file\n", 26);
 	if (!(fd = open(argv[1], O_RDONLY)))
 	{
-		write(2, "Open failed work with file", path);
+		write(2, "Open failed work with file\n", 26);
 		return(-1);
 	}
 	return(0);
