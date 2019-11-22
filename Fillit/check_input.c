31f022fe677f9addf8d4f6322bc92c466ef6e521
Lilian Harvey <lharvey@student.hive.fi>
2019-11-22 17:29:27 +0200

    Modified Check input to work gooder. Main modified for testing

--- Fillit/check_input.c
+++ Fillit/check_input.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 11:34:46 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/22 17:17:12 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/22 17:28:53 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 static int	line_checker(char *map)
 {
-	unsigned char str_length;
-	unsigned char tetri_height;
-	unsigned char num_tetri;
+	unsigned char	str_length;
+	unsigned char	tetri_height;
+	unsigned char	num_tetri;
 
 	str_length = 0;
 	num_tetri = 0;
 	tetri_height = 0;
-	while ((*map == '#' || *map == '.') && (str_length++ < 6) 
+	while ((*map == '#' || *map == '.') && (str_length++ < 6)
 			&& (num_tetri <= 26 && (tetri_height <= 4)))
 	{
 		if (str_length == 5 && *map == '\n')
@@ -31,12 +31,12 @@ static int	line_checker(char *map)
 			str_length = 0;
 			tetri_height++;
 		}
-		if (str_length == 0 && *map == '\n' 
-				&& *(map + 1) != '#' || *(map + 1) != '.')
+		if (str_length == 0 && *map == '\n'
+				&& (*(map + 1) != '#' || *(map + 1) != '.'))
 			return (0);
 		if (str_length == 0 && *map == '\n')
 		{
-		   	tetri_height = 0;
+			tetri_height = 0;
 			num_tetri++;
 		}
 		if (*map == '\0')
@@ -46,26 +46,23 @@ static int	line_checker(char *map)
 				num_tetri++;
 				return (1);
 			}
-			else 
-				return (0):
+			else
+				return (0);
 		}
 		map++;
 	}
-	else 
-		return (0);
+	return (0);
 }
 
-int		check_input(int fd)
+int			check_input(int fd)
 {
-	char map[BUFF_SIZE];
-	int ret;
-	int i;
+	char	map[BUFF_SIZE];
+	int		ret;
+	int		line_checker_ret;
 
 	if ((ret = read(fd, map, BUFF_SIZE) < 0))
 		return (0);
-	int length = linelength(map);
-	printf("line length is: %d\n", length);
-	if (length != 4)
-		return (0);
+	line_checker(map);
+	printf("line checker says: %d\n", line_checker_ret);
 	return (1);
-}		
+}
--- Fillit/test_main.c
+++ Fillit/test_main.c
@@ -1,4 +1,3 @@
-#include "print_binary.h"
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -6,9 +5,9 @@ int		check_input(int fd);
 
 int main()
 {
-	int fd = open("test_files/1_NoEOF", O_RDONLY);
+	int fd = open("../Test_Files_TBR!/1_NoEOF", O_RDONLY);
 	int function_ret = check_input(fd);
 	printf("open returned: %d\nfunction returned: %d\n", fd, function_ret);
 
-	return 0;	
+	return (0);	
 }
