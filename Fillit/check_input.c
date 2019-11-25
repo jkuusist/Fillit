bf9fe1fc60de92b89f68cad2ab0fa9ede2a375a5
Lilian Harvey <lharvey@student.hive.fi>
2019-11-25 12:58:23 +0200

    Smoother Operator

--- Fillit/check_input.c
+++ Fillit/check_input.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 11:34:46 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/25 12:17:18 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/25 12:57:56 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,36 +24,32 @@ static int	line_checker(char *map)
 	num_tetri = 0;
 	tetri_height = 0;
 	while ((*map == '#' || *map == '.' || *map == '\n' || *map == '\0') 
-			&& (str_length++ < 6) && (num_tetri <= 26 && (tetri_height <= 4)))
+			&& (line_length++ < 6) && (num_tetri <= 26 && (tetri_height <= 4)))
 	{
-		if (str_length == 0 && *map == '\n')
-		{
-			if ((*(map + 1) != '#') && (*(map + 1) != '.') 
-					&& (*(map + 1) != '\0'))
-				return (0);
-			else
-			{
-				tetri_height = 0;
-				num_tetri++;
-			}
-		}
 		if (*map == '\0')
 		{
-			if (str_length == 5 && tetri_height == 4)
+			if (line_length == 5 && tetri_height == 4)
 			{
 				num_tetri++;
 				return (1);
 			}
-			if (num_tetri && (str_length == 1 && tetri_height == 0))
+			if (num_tetri && (line_length == 2 && tetri_height == 0))
 				return (1);
 			else
 				return (0);
 		}
-		if (str_length == 5 && *map == '\n')
+		if (line_length == 1 && *map == '\n' && tetri_height == 4)
 		{
-			str_length = 0;
+			tetri_height = 0;
+			num_tetri++;
+		}
+		if (line_length == 5 && *map == '\n')
+		{
+			line_length = 0;
 			tetri_height++;
 		}
+		if (*map == '\n' && (line_length != 1 || line_length != 5))
+			return (0);
 		map++;
 	}
 	return (0);
