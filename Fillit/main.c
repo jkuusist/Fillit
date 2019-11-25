23e8cf261e428dc595d956863417affe9ef7252a
jkuusist <jkuusist@student.hive.fi>
2019-11-25 11:07:20 +0200

    changed main.c

--- Fillit/main.c
+++ Fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 11:21:04 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/22 11:40:50 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/25 11:06:18 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int		main(int argc, char **argv)
 		write(2, "Open failed work with file\n", 26);
 		return (-1);
 	}
+	int function_ret = check_input(fd);
+	printf("open returned: %d\ncheck_input returned: %d\n", fd, function_ret);
 	close(fd);
 	return (0);
 }
