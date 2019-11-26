be207310345e3f876e11b9c5e6377aa613f61cdf
Lilian Harvey <lharvey@student.hive.fi>
2019-11-26 16:57:45 +0200

    Added Bitwise Checks for Left and Up of a 4x4 bitmap

--- Fillit/check_left.c
+++ Fillit/check_left.c
@@ -6,18 +6,23 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 11:45:19 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/26 16:31:41 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/26 16:55:20 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	check_left(unsigned char *semi_valid_map)
+int		check_left(unsigned char *semi_valid_map)
 {
 	unsigned int	leftmost_mask;
 
 	leftmost_mask = 34952;	
-	
 	if (*semi_valid_map & leftmost_mask >= 1)
 		return (1);
 	else
 		return (0);
+}
+
+void	shift_left(unsigned char *semi_valid_map)
+{
+	while (check_left(semi_valid_map) == 1)
+		*semi_valid_map <<= 1;
 }
--- Fillit/check_up.c
+++ Fillit/check_up.c
@@ -6,7 +6,7 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 11:59:39 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/26 16:19:57 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/26 16:57:18 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@ int	check_up(unsigned char *semi_valid_map)
 		return (1);
 	else 
 		return (0);
+}
+
+void	shift_up(unsigned char *semi_valid_map)
+{
+	while (check_up(semi_valid_map) == 1)
+		*semi_valid_map <<= 4;
 }
