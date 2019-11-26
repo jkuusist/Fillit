76707e8651b99093aa556bcbf09a5c82b114007f
jkuusist <jkuusist@student.hive.fi>
2019-11-26 16:31:52 +0200

    updated header

--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 10:32:01 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/26 10:35:39 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/26 16:30:32 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define FILLIT_H
 
 int				check_input(int fd);
-unsigned char	**create_block_array(char *string, int num_blocks);
+unsigned int	*create_block_array(char *string, int num_blocks);
 int				binary_checker(void *a_tetrino);
+unsigned int	str_to_bin(unsigned char *block);
 
 #endif
