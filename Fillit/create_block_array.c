7dd1f04fe7e9579172a18dc7aacc0c49330f3a30
jkuusist <jkuusist@student.hive.fi>
2019-11-26 16:51:10 +0200

    create_block_array changed to int

--- Fillit/create_block_array.c
+++ Fillit/create_block_array.c
@@ -6,36 +6,41 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:17:19 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/25 17:04:08 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/26 16:50:26 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "fillit.h"
+#include "../Libft/libft.h"
 
-unsigned char	**create_block_array(char *string, int num_blocks)
+unsigned int	*create_block_array(char *string, int num_blocks)
 {
 	int				i;
 	int				j;
 	int				nl_count;
-	unsigned char	**array;
+	unsigned char	*temp;
+	unsigned int	*array;
 
 	i = 0;
 	j = 0;
 	nl_count = 0;
-	array = (unsigned char**)malloc(sizeof(unsigned char**) * 26);
+	array = (unsigned int*)malloc(sizeof(unsigned int*) * 26);
 	while (i < num_blocks)
 	{
-		array[i] = (unsigned char *)malloc((sizeof(unsigned char*)) * 21);
+		temp = (unsigned char *)malloc((sizeof(unsigned char*)) * 21);
 		while (*string != '\0' && nl_count < 5)
 		{
 			if (*string == '\n')
 				nl_count++;
-			array[i][j++] = *string;
+			temp[j++] = *string;
 			string++;
 		}
-		array[i++][j - 1] = '\0';
+		array[i] = str_to_bin(temp);
+		ft_strdel((char**)&temp);
 		j = 0;
 		nl_count = 0;
+		i++;
 	}
 	array[i] = 0;
 	return (array);
