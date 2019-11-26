43062c5ead0ab2ecfd030d217780907984f3a053
Lilian Harvey <lharvey@student.hive.fi>
2019-11-26 16:43:20 +0200

    Added check for leftmost bits in a 4x4 array

--- Fillit/check_left.c
+++ Fillit/check_left.c
@@ -6,20 +6,17 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 11:45:19 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/26 11:57:01 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/26 16:31:41 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	check_left(unsigned char *semi_valid_map)
 {
-	int	i;
-	int	empty_edge;
+	unsigned int	leftmost_mask;
 
-	i = 0;
-	empty_edge = 0;
-	while (semi_valid_map[i] == '.' && empty_edge++ < 4)
-		i + 5;
-	if (empty_edge == 4)
+	leftmost_mask = 34952;	
+	
+	if (*semi_valid_map & leftmost_mask >= 1)
 		return (1);
 	else
 		return (0);
