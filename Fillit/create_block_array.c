e209af8ae72df8e0f216b73414f30377abd96f44
jkuusist <jkuusist@student.hive.fi>
2019-11-27 16:49:37 +0200

    changed cba to only take one argument

--- Fillit/create_block_array.c
+++ Fillit/create_block_array.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:17:19 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/27 12:43:41 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/27 16:49:06 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,43 @@ void			write_temp(char **string, unsigned char *temp, int *nl_count)
 	}
 }
 
-unsigned int	*create_block_array(char *string, int num_blocks)
+int				get_block_count(char *string)
+{
+	int	i;
+	int	block_count;
+
+	i = 0;
+	block_count = 0;
+	while (string[i] != '\0')
+	{
+		if (string[i] == '\n')
+			block_count++;
+		i++;
+	}
+	if (block_count != 0)
+		block_count++;
+	return (block_count / 5);
+}
+
+unsigned int	*create_block_array(char *string)
 {
 	int				i;
-	int				j;
+	int				block_count;
 	int				nl_count;
 	unsigned char	*temp;
 	unsigned int	*array;
 
 	i = 0;
-	j = 0;
+	block_count = get_block_count(string);
 	nl_count = 0;
 	array = (unsigned int*)malloc(sizeof(unsigned int*) * 27);
-	while (i < num_blocks)
+	while (i < block_count)
 	{
 		temp = (unsigned char *)malloc((sizeof(unsigned char*)) * 21);
 		write_temp(&string, temp, &nl_count);
 		string++;
 		array[i] = str_to_bin(temp);
 		ft_strdel((char**)&temp);
-		j = 0;
 		nl_count = 0;
 		i++;
 	}
