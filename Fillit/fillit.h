e1669d26f588eb1e54c51a6717bf86f506293963
Lilian Harvey <lharvey@student.hive.fi>
2019-11-27 14:27:11 +0200

    Added header file

--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 10:32:01 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/26 16:30:32 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/27 14:24:27 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 # define FILLIT_H
 
 int				check_input(int fd);
-unsigned int	*create_block_array(char *string, int num_blocks);
-int				binary_checker(void *a_tetrino);
+unsigned int	*create_block_array(char *string);
+int				binary_checker(unsigned int *semi_valid_array);
 unsigned int	str_to_bin(unsigned char *block);
 
 #endif
