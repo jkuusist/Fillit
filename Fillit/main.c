31be5185c5cb84961880ecf2c8da1d582f479cf2
jkuusist <jkuusist@student.hive.fi>
2019-11-27 14:26:02 +0200

    fixes to main.c

--- Fillit/main.c
+++ Fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 11:21:04 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/27 13:50:39 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/27 14:25:41 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ unsigned int	*check_validity(char *file_map)
 
 	semi_valid_array = create_block_array(file_map);
 	if (binary_checker(semi_valid_array))
-		return (1);
+		return (semi_valid_array);
 	return (0);
 }
 
@@ -29,7 +29,7 @@ int				main(int argc, char **argv)
 {
 	int				fd;
 	char			*file_map;
-	unsigned int	*binary_map
+	unsigned int	*binary_map;
 
 	if (argc != 2)
 	{
@@ -41,9 +41,9 @@ int				main(int argc, char **argv)
 		write(2, "Open failed work with file\n", 26);
 		return (-1);
 	}
-	if (*file_map = (check_input(fd) == NULL))
+	if (!(*file_map = check_input(fd)))
 		write(1, "error\n", 6);
-	else if (*binary_map = (check_validity(file_map)))
+	else if ((binary_map = check_validity(file_map)))
 		write(1, "Algorithm_Nonsence_That_Needs_Creating\n", 39);
 	else
 		write(1, "error\n", 6);
