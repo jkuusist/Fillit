372252cf3a99ca9e9a6525267347ac419e0276d1
Lilian Harvey <lharvey@student.hive.fi>
2019-11-27 12:55:26 +0200

    Combined shift functions into the binary checker file, to be used at a later date

--- Fillit/binary_checker.c
+++ Fillit/binary_checker.c
@@ -6,11 +6,15 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 14:56:47 by lharvey           #+#    #+#             */
-/*   Updated: 2019/11/27 12:21:10 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/11/27 12:53:08 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	binary_checker(unsigned int *tetrino_block)
+/*
+** Decimal representations of each (Top & Leftmost version) valid tetrino.
+*/
+
+int		valid_maps(unsigned int *tetrino_block)
 {
 	if (*tetrino_block != 52224 || *tetrino_block != 58368 ||
 		*tetrino_block != 19520 || *tetrino_block != 35968 ||
@@ -25,4 +29,28 @@ int	binary_checker(unsigned int *tetrino_block)
 		return (0);
 	else
 		return (1);
+}
+
+/*
+** Shift functions move the bitmaps up/left via the magic of bit masks.
+*/
+
+void	shift_left(unsigned int *semi_valid_map)
+{
+	unsigned int	leftmost_mask;
+
+	leftmost_mask = 34952;
+	while (*semi_valid_map & leftmost_mask >= 1)
+		*semi_valid_map <<= 1;
+}
+
+void	shift_up(unsigned int *semi_valid_map)
+{
+	while (*semi_valid_map < 4096)
+		*semi_valid_map <<= 4;
+}
+
+int		binary_checker(unsigned int **semi_valid_array)
+{
+	return (0);
 }
