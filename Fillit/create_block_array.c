556505621810cbcca7b52d68e7331beaa0c77312
jkuusist <jkuusist@student.hive.fi>
2019-11-27 13:31:12 +0200

    slight tweaks to create_block_array

--- Fillit/create_block_array.c
+++ Fillit/create_block_array.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:17:19 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/27 11:00:32 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/27 12:43:41 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void			write_temp(char **string, unsigned char *temp, int *nl_count)
 	int i;
 
 	i = 0;
-	while (**string != '\0' && *nl_count < 5)
+	while (**string != '\0' && *nl_count < 6)
 	{
 		if (**string == '\n')
 			nl_count++;
@@ -39,7 +39,7 @@ unsigned int	*create_block_array(char *string, int num_blocks)
 	i = 0;
 	j = 0;
 	nl_count = 0;
-	array = (unsigned int*)malloc(sizeof(unsigned int*) * 26);
+	array = (unsigned int*)malloc(sizeof(unsigned int*) * 27);
 	while (i < num_blocks)
 	{
 		temp = (unsigned char *)malloc((sizeof(unsigned char*)) * 21);
