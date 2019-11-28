3d0fc3f2e02ab8c15de58b82b2c9d02672849d5b
jkuusist <jkuusist@student.hive.fi>
2019-11-28 14:21:52 +0200

    removed one layer of pointers, plus fixed nl_count to be inceremented properly

--- Fillit/create_block_array.c
+++ Fillit/create_block_array.c
@@ -6,25 +6,28 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:17:19 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/28 13:44:54 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/11/28 14:19:55 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "fillit.h"
 #include "../Libft/libft.h"
+#include <stdio.h>
 
-void			write_temp(unsigned char **string, unsigned char *temp, int *nl_count)
+void			write_temp(unsigned char *string, unsigned char *temp, int *nl_count)
 {
+	//printf("string passed to temp is: %s", *string);
 	int i;
 
 	i = 0;
-	while (**string != '\0' && *nl_count < 6)
+	while (*string != '\0' && *nl_count < 6)
 	{
-		if (**string == '\n')
-			nl_count++;
-		temp[i++] = **string;
-		(*string)++;
+		if (*string == '\n')
+			(*nl_count)++;
+		//printf("nl_count is: %d current char is: %c\n", *nl_count, **string);
+		temp[i++] = *string;
+		string++;
 	}
 }
 
@@ -61,9 +64,15 @@ unsigned int	*create_block_array(unsigned char *string)
 	while (i < block_count)
 	{
 		temp = (unsigned char *)malloc((sizeof(unsigned char*)) * 21);
-		write_temp(&string, temp, &nl_count);
+		write_temp(string, temp, &nl_count);
 		string++;
+
+		printf("temp is: %s", temp);
+
 		array[i] = str_to_bin(temp);
+
+		printf("array[i] is: %d\n", array[i]);
+
 		ft_strdel((char**)&temp);
 		nl_count = 0;
 		i++;
