5784b66de0b021f721c4e02b9f9eb3f35a471d95
Lilian Harvey <lharvey@student.hive.fi>
2019-12-02 17:49:21 +0200

    initialised the array of bits

--- Fillit/solver.c
+++ Fillit/solver.c
@@ -6,18 +6,27 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 11:42:24 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/02 14:29:56 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/02 17:49:07 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** (13 x 13) is max array (169 bits) - (11 * 2 * 8  == 176)
+*/
+
 void	*solver(unsigned short *binary_map)
 {
-	int i;
-	int map_size;
+//	int i;
+//	int map_size;
+	unsigned short bit_field[10];
+	/* FREE THE BITFIELD OF JUNK I BEG YE */
+	while (i <= 10)
+		ft_memset(bit_field[i++], 0, ((sizeof(unsigned short)) * 8))
+//	i = 0;
+//	while (binary_map[i] != 0)
+//		i++;
+//	map_size = squaireroot(i * 4);
 
-	i = 0;
-	while (binary_map[i] != 0)
-		i++;
-	map_size = squareroot(i * 4);
+
 
 }
