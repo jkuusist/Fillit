b63a175775cce52726156a1fb4b0fed0a8793fe3
Lilian Harvey <lharvey@student.hive.fi>
2019-12-05 10:41:44 +0200

    Be nice to your terminal

--- Fillit/solver.c
+++ Fillit/solver.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 11:42:24 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/04 13:33:23 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/05 10:41:32 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,17 @@ static void				clear_bit(unsigned short *bit_field,
 
 void					*solver(unsigned short *binary_map)
 {
-	int i;
+	unsigned int i;
 	unsigned short bit_field[10];
-//	int map_size;
+	unsigned int min_size;
 
 	while (i <= 10)
 		ft_memset(bit_field[i++], 0, 1);
-//	i = 0;
-//	while (binary_map[i] != 0)
-//		i++;
-//	map_size = squaireroot(i * 4);
+	i = 0;
+	while (binary_map[i] != 0)
+		i++;
+	min_size = (unsigned int)squareroot(i * 4);
+	ft_putnbr((int)min_size);
+	// PUT THE HECKIN TETRINO FROM MAP TO FIELD
+	
 }
