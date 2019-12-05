898ef0674dc1f6d1e30e00d76ef98ff853a48b70
jkuusist <jkuusist@student.hive.fi>
2019-12-05 14:45:21 +0200

    fixed create_tblock thing?

--- Fillit/create_tblocks.c
+++ Fillit/create_tblocks.c
@@ -6,26 +6,26 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 12:30:28 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/05 13:43:35 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/12/05 14:44:44 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "fillit.h"
 
-void	tblock_conversion(unsigned short *binary_map, t_block *bit_field
+void	tblock_conversion(unsigned short *binary_map, unsigned short *array,
 		int j)
 {
 			if (j == 0)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 61440);
+				array[j] = (binary_map[j] | 61440);
 			else if (j == 1)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 3840);
+				array[j] = (binary_map[j] | 3840);
 			else if (j == 2)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 240);
+				array[j] = (binary_map[j] | 240);
 			else if (j == 3)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 15);
+				array[j] = (binary_map[j] | 15);
 			else
-				bit_field[j].tetrino_field[j] = 0;
+				array[j] = 0;
 }
 
 t_block	*create_block(unsigned short *binary_map)
@@ -35,20 +35,23 @@ t_block	*create_block(unsigned short *binary_map)
 	unsigned char	id;
 	t_block			temp;
 	t_block			*bit_field;
+	unsigned short	array[10];
 
 	i = 0;
 	j = 0;
 	id = 'A';
-	bit_field = malloc(sizeof(t_block) * 10);
+	bit_field = (t_block*)malloc(sizeof(t_block) * 10);
 	while (binary_map[i] != 0)
 	{
 		temp.id = id;
-		temp.tetrino_field = malloc(sizeof(unsigned short) * 10);
 		while (j < 10)
 		{
-			tblock_conversion(binary_map, bit_field, j);	
+			tblock_conversion(binary_map, array, j);
 			j++;
 		}
+		temp.tetrino_field = &array;
+//ZERO ARRAY BEFOER NEXT LOOP
+
 		bit_field[i] = temp;
 		id++;
 		i++;
