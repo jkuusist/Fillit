992d8b5f4e704365ccb3d3b9061c4af5431197c0
Lilian Harvey <lharvey@student.hive.fi>
2019-12-05 11:08:51 +0200

    Consolidated files and updated headers

--- Fillit/create_block_array.c
+++ Fillit/create_block_array.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:17:19 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/11/29 13:54:33 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/05 11:05:47 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,54 @@
 #include "fillit.h"
 #include "../Libft/libft.h"
 
-static void		write_temp(unsigned char **string, unsigned char *temp,
-				int *nl_count)
+static unsigned short		binary_atoi(const unsigned char *str, int len)
+{
+	unsigned long long	binary_value;
+	unsigned long long	result;
+
+	binary_value = 1;
+	result = 0;
+	len--;
+	while ((str[len] == '0' || str[len] == '1') && len >= 0)
+	{
+		if (str[len] == '1')
+			result += binary_value;
+		binary_value *= 2;
+		len--;
+	}
+	return (result);
+}
+
+static unsigned short		str_to_bin(unsigned char *block)
+{
+	int				i;
+	int				length;
+	unsigned char	*binstr;
+	unsigned short	result;
+
+	i = 0;
+	length = 0;
+	binstr = (unsigned char *)malloc(sizeof(unsigned char) * 16);
+	while (block[i] != '\0')
+	{
+		if (block[i] == '#')
+		{
+			ft_strcat((char*)binstr, "1");
+			length++;
+		}
+		if (block[i] == '.')
+		{
+			ft_strcat((char*)binstr, "0");
+			length++;
+		}
+		i++;
+	}
+	result = binary_atoi(binstr, length);
+	free(binstr);
+	return (result);
+
+static void					write_temp(unsigned char **string, 
+		unsigned char *temp, int *nl_count)
 {
 	int i;
 
@@ -32,7 +78,7 @@ static void		write_temp(unsigned char **string, unsigned char *temp,
 	}
 }
 
-static int		get_block_count(unsigned char *string)
+static int					get_block_count(unsigned char *string)
 {
 	int	i;
 	int	block_count;
@@ -50,7 +96,7 @@ static int		get_block_count(unsigned char *string)
 	return (block_count / 5);
 }
 
-unsigned short	*create_block_array(unsigned char *string)
+unsigned short				*create_block_array(unsigned char *string)
 {
 	int				i;
 	int				block_count;
--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 10:32:01 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/05 10:14:26 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/12/05 11:05:23 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 unsigned char		*check_input(int fd);
 unsigned short		*create_block_array(unsigned char *string);
 unsigned short		*binary_checker(unsigned short *semi_valid_array);
-unsigned short		str_to_bin(unsigned char *block);
 unsigned long long	squareroot(unsigned long long x);
 unsigned short		shift_left(unsigned short *bit_field, int shift_amount);
 unsigned short		shift_right(unsigned short *bit_field, int shift_amount);
