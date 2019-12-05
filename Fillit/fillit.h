7ca8bc07db1b05ddf94b9f15954cea3a9c12b5de
Lilian Harvey <lharvey@student.hive.fi>
2019-12-05 10:18:46 +0200

    Initial commit for algorithim integration

--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,17 +6,23 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 10:32:01 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/04 17:17:20 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/12/05 10:14:26 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-unsigned char	*check_input(int fd);
-unsigned short	*create_block_array(unsigned char *string);
-unsigned short	*binary_checker(unsigned short *semi_valid_array);
-unsigned short	str_to_bin(unsigned char *block);
+unsigned char		*check_input(int fd);
+unsigned short		*create_block_array(unsigned char *string);
+unsigned short		*binary_checker(unsigned short *semi_valid_array);
+unsigned short		str_to_bin(unsigned char *block);
 unsigned long long	squareroot(unsigned long long x);
+unsigned short		shift_left(unsigned short *bit_field, int shift_amount);
+unsigned short		shift_right(unsigned short *bit_field, int shift_amount);
+unsigned short		shift_up(unsigned short *bit_field, int shift_amount);
+unsigned short		shift_down(unsigned short *bit_field, int shift_amount);
+unsigned short		*R_SHIFT_array_create(void);
+unsigned short		*L_SHIFT_array_create(void);
 
 #endif
