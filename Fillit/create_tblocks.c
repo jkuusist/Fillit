bf3e0a21d7b0f76ceedae4ce8561ed50747c60b6
Lilian Harvey <lharvey@student.hive.fi>
2019-12-05 13:45:27 +0200

    Fixed Norm errors a bit

--- Fillit/create_tblocks.c
+++ Fillit/create_tblocks.c
@@ -6,12 +6,27 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 12:30:28 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/05 13:36:54 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/05 13:43:35 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "fillit.h"
+
+void	tblock_conversion(unsigned short *binary_map, t_block *bit_field
+		int j)
+{
+			if (j == 0)
+				bit_field[j].tetrino_field[j] = (binary_map[j] | 61440);
+			else if (j == 1)
+				bit_field[j].tetrino_field[j] = (binary_map[j] | 3840);
+			else if (j == 2)
+				bit_field[j].tetrino_field[j] = (binary_map[j] | 240);
+			else if (j == 3)
+				bit_field[j].tetrino_field[j] = (binary_map[j] | 15);
+			else
+				bit_field[j].tetrino_field[j] = 0;
+}
 
 t_block	*create_block(unsigned short *binary_map)
 {
@@ -31,16 +46,7 @@ t_block	*create_block(unsigned short *binary_map)
 		temp.tetrino_field = malloc(sizeof(unsigned short) * 10);
 		while (j < 10)
 		{
-			if (j == 0)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 61440);
-			else if (j == 1)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 3840);
-			else if (j == 2)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 240);
-			else if (j == 3)
-				bit_field[j].tetrino_field[j] = (binary_map[j] | 15);
-			else
-				bit_field[j].tetrino_field[j] = 0;
+			tblock_conversion(binary_map, bit_field, j);	
 			j++;
 		}
 		bit_field[i] = temp;
