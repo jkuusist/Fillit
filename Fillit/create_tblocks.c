1e66ab957e6f9d3fab9fb1a59675ebeb3caae45c
Lilian Harvey <lharvey@student.hive.fi>
2019-12-09 13:53:11 +0200

    Im back, baby

--- Fillit/create_tblocks.c
+++ Fillit/create_tblocks.c
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 12:30:28 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/09 13:31:45 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/09 13:51:21 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,69 +17,63 @@
 //DELET THIS
 #include <stdio.h>
 
-/*
-void	tblock_conversion(unsigned short *binary_map, unsigned short *array,
-		int j)
+void	stack_to_heap(unsigned short *binary_map, unsigned short *array)
 {
-			if (j == 0)
-				array[j] = (binary_map[j] & 61440);
-			else if (j == 1)
-				array[j] = (binary_map[j] & 3840);
-			else if (j == 2)
-				array[j] = (binary_map[j] & 240);
-			else if (j == 3)
-				array[j] = (binary_map[j] & 15);
-			else
-				array[j] = 0;
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		temp->tetrino_field[i] = array[i++];
+	}
 }
-*/
+
+void	tblock_conversion(unsigned short *binary_map, unsigned short *array)
+{
+	int	j;
+
+	j = 0;
+	while (j < 10)
+	{
+		if (j == 0)
+			array[j] = (binary_map[j] & 61440);
+		else if (j == 1)
+			array[j] = (binary_map[j] & 3840);
+		else if (j == 2)
+			array[j] = (binary_map[j] & 240);
+		else if (j == 3)
+			array[j] = (binary_map[j] & 15);
+		else
+			array[j] = 0;
+	}
+	stack_to_heap(binary_map, array)
+}
+
 t_block	**create_tblock(unsigned short *binary_map)
 {
 	int				i;
-	int				j;
 	unsigned char	id;
 	t_block			*temp;
 	t_block			**bit_field;
 	unsigned short	array[10];
 
 	i = 0;
-	j = 0;
 	id = 'A';
 	bit_field = (t_block**)malloc(sizeof(t_block) * 10);
 	while (binary_map[i] != 0)
 	{
 		bit_field[i] = (t_block*)malloc(sizeof(t_block));
-		bit_field[i]->tetrino_field = malloc(sizeof(unsigned short) * 10);
 		temp = (t_block*)malloc(sizeof(t_block));
+		temp->(*tetrino_field) = (unsigned short*)malloc(sizeof(unsigned short) * 10);
 		temp->id = id;
-		while (j < 10)
-		{
-			//tblock_conversion(binary_map, array, j);
-
-			if (j == 0)
-				array[j] = (binary_map[j] & 61440);
-			else if (j == 1)
-				array[j] = (binary_map[j] & 3840);
-			else if (j == 2)
-				array[j] = (binary_map[j] & 240);
-			else if (j == 3)
-				array[j] = (binary_map[j] & 15);
-			else
-				array[j] = 0;
-			j++;
-		}
-		temp->tetrino_field = array;
+		tblock_conversion(binary_map, array);
 //ZERO ARRAY BEFOER NEXT LOOP
 		
 		printf("temp->id is: %c\n", temp->id);
-
 		bit_field[i] = temp;
-
 		printf("bit_field[%d]->id is: %c\n", i, bit_field[i]->id);
 
 		ft_bzero(array, 10);
-		j = 0;
-		free(temp);
 		id++;
 		i++;
 	}
