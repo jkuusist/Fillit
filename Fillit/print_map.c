fbf1b3467b98d1ffe7d61e55825a822fc3f74a6a
Lilian Harvey <lharvey@student.hive.fi>
2019-12-12 14:13:57 +0200

    Print map skeleton completed

--- Fillit/print_map.c
+++ Fillit/print_map.c
@@ -6,37 +6,41 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 11:45:18 by lharvey           #+#    #+#             */
-/*   Updated: 2019/12/12 13:58:23 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/12/12 14:13:36 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	print_char(char *charmap, char c, int index)
-{
-	charmap[i] = c;
-}
 
 void	print_map(t_block **bit_field, unsigned int map_size, unsigned int num_tetrinos)
 {
 	char	char_map[(map_size*map_size) + map_size];
 	int i;
 	int checked_bits;
+	int checked_rows;
 
 	i = 0;
 	checked_bits = 0;
+	checked_rows = 0;
 	while (i <= num_tetrinos)
 		ft_memset((&(char_map[i++])), '.', 1)
 	i = 0;
 	while (bit_field[i] != NULL)
 	{
-		while ((checked_bits <= 16) && (checked_rows < 10))
+		while (checked_rows <= map_size)
 		{
-//			check every bit for 1
-//			if 1, put -> ID in that charmap[i]
-//			if 0, ++, until an entire t_block is cleared.
+			if (checked bits / 16 == 0)
+			{
+				checked_rows++;
+				checked_bits = 0;
+			}
+			if (checked_bits / map_size == 0)
+				char_map[i++] = '\n';
+			if (check_bit(bit_field[i]->tetrino_field[checked_rows], checked_bits++))
+			{
+				char_map[i] = bit_field[i]->id; 
+				i++;
+			}
 		}
-//	Go to the next t_block
-	i++;
+		i++;
 	}
-//	Print the final result
 	ft_putstr(charmap);
 }
