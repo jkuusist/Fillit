d12d70c7dcd8c2a70f732de54e1dbe399246fd8e
jkuusist <jkuusist@student.hive.fi>
2019-12-12 13:32:02 +0200

    removed uint8_t and updated makefile

--- Libft/print_binary.c
+++ Libft/print_binary.c
@@ -6,23 +6,23 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 11:01:49 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/05 11:15:29 by lharvey          ###   ########.fr       */
+/*   Updated: 2019/12/12 13:28:07 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	print_binary(uint8_t byte)
+void	print_binary(unsigned char byte)
 {
-	int		i;
-	uint8_t	str[9];
+	int				i;
+	unsigned char	str[9];
 
 	i = 128;
 	str[0] = '\0';
 	while (i > 0)
 	{
-		ft_strcat(str, ((byte & i) == i) ? "1" : "0");
+		ft_strcat((char*)str, ((byte & i) == i) ? "1" : "0");
 		i >>= 1;
 	}
-	ft_putstr(str);
+	ft_putstr((char*)str);
 }
