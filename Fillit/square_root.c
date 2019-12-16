d9a423d222c18fb70856db9cf11760cafafe8fae
Lilian Harvey <lharvey@student.hive.fi>
2019-12-16 17:03:41 +0200

    Added sqrt fn

---
+++ Fillit/square_root.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   square_root.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/16 16:26:56 by lharvey           #+#    #+#             */
+/*   Updated: 2019/12/16 16:28:02 by lharvey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned long long	square_root(unsigned long long x)
+{
+	unsigned long long start;
+	unsigned long long end;
+	unsigned long long result;
+	unsigned long long mid;
+
+	if (!x || x > 18446744073709551615U)
+		return (0);
+	if (x == 0 || x == 1)
+		return (x);
+	start = 1;
+	end = x;
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (mid * mid == x)
+			return (mid);
+		if (mid * mid < x)
+		{
+			start = mid + 1;
+			result = mid;
+		}
+		else
+			end = mid - 1;
+	}
+	return (result);
+}
