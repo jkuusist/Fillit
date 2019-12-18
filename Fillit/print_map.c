48129391f744eafe88fd29bf691db201b0764527
Lilian Harvey <lharvey@student.hive.fi>
2019-12-18 16:58:26 +0200

    Print map refractored and now Norm compliant

--- Fillit/print_map.c
+++ Fillit/print_map.c
@@ -6,7 +6,7 @@
 /*   By: lharvey <lharvey@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 11:45:18 by lharvey           #+#    #+#             */
-/*   Updated: 2019/12/18 15:58:06 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/18 16:57:55 by lharvey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ static unsigned short		check_bit(unsigned short tetrino_field,
 		return (0);
 }
 
-unsigned int				check_four(unsigned short *map_field, unsigned int map_size)
+unsigned int				check_four(unsigned short *map_field,
+		unsigned int map_size)
 {
 	unsigned int	i;
-	
+
 	i = 0;
 	while ((map_size < 5) && (i < 5))
 	{
@@ -40,51 +41,45 @@ unsigned int				check_four(unsigned short *map_field, unsigned int map_size)
 	return (map_size);
 }
 
+static unsigned int			fill_charmap(unsigned int map_size,
+		t_block **bit_field, char *char_map, unsigned int i)
+{
+	unsigned int	j;
+	unsigned int	checked_bits;
+	unsigned int	checked_rows;
+
+	j = 0;
+	checked_bits = 0;
+	checked_rows = 0;
+	while (checked_rows < map_size)
+	{
+		if (check_bit(bit_field[i]->tetrino_field
+					[checked_rows], checked_bits++))
+			char_map[j++] = bit_field[i]->id;
+		else
+			j++;
+		if (checked_bits == map_size)
+		{
+			checked_rows++;
+			char_map[j++] = '\n';
+			checked_bits = 0;
+		}
+	}
+	return (j);
+}
+
 void						print_map(t_block **bit_field,
 		unsigned int map_size, unsigned int num_tetrinos)
 {
 	char			char_map[((map_size * map_size) + map_size) + 1];
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	checked_bits;
-	unsigned int	checked_rows;
-/*
-	for (int o = 0; o < 10; o++)
-		for (int p = 0; p < 10; p++)
-			printf("bit_field[%d]->tetrino_field[%d] is %d\n", o, p, bit_field[o]->tetrino_field[p]);
-	printf("\n");	
-*/
+
 	i = 0;
-	checked_bits = 0;
-	checked_rows = 0;
 	ft_memset(char_map, '.', ((map_size * map_size) + map_size));
 	while ((bit_field[i] != NULL) && (i < num_tetrinos))
 	{
-		j = 0;
-		while ((checked_rows < map_size) && (i < num_tetrinos))
-		{
-			if (check_bit(bit_field[i]->tetrino_field[checked_rows],
-						checked_bits))
-			{
-				char_map[j] = bit_field[i]->id;
-				checked_bits++;
-				j++;
-			}
-			else
-			{
-				checked_bits++;
-				j++;
-			}
-			if (checked_bits == map_size)
-			{
-				char_map[j] = '\n';
-				j++;
-				checked_rows++;
-				checked_bits = 0;
-			}
-		}
-		checked_rows = 0;
-		checked_bits = 0;
+		j = fill_charmap(map_size, bit_field, char_map, i);
 		i++;
 	}
 	char_map[j] = '\0';
