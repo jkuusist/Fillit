ca5bbf7dda39099da2e1b20814a578cbc7be9dff
jkuusist <jkuusist@student.hive.fi>
2019-12-18 12:40:10 +0200

    added shifter to header

--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jkuusist <jkuusist@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 10:32:01 by jkuusist          #+#    #+#             */
-/*   Updated: 2019/12/16 17:11:32 by jkuusist         ###   ########.fr       */
+/*   Updated: 2019/12/18 12:03:22 by jkuusist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,7 @@ t_block				**copy_tblocks(t_block **src);
 void				free_tblocks(t_block **array);
 unsigned long long	square_root(unsigned long long x);
 unsigned int		check_four(unsigned short *map_field, unsigned int map_size);
+unsigned short	shifter(unsigned short *map_field, int shift_amount,
+	   	unsigned int mask_size);
 
 #endif
